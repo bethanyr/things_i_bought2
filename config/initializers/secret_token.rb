546d4e90e5855d122d268bd862f407f752b0fa2b
bethanyr <bethany.rentz@hotmail.com>
2013-07-11 17:43:43 -0700

    initial files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThingsIBought2::Application.config.secret_token = '4d7ffa01b97d2ecf24090748eae15d871b3f0863bf6548619db65f89fa7bfd47083f193b30f0d3b0acc64454937b81cab5f7f82bcfe583733102d3766e96eab2'
